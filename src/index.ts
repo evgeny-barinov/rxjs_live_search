a3732c867b497207e67a91f555c8db267a0d483a
Evgeniy Barinov <z.barinov@gmail.com>
2019-02-15 19:58:01 +0300

    fixes. seems to work perfect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {from, fromEvent, Observable, of} from 'rxjs';
-import {catchError, debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
+import { from, fromEvent, Observable, of } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
 
 interface IGithubUrl {
   name: string;
@@ -11,37 +11,48 @@ interface IGithubResponse {
 }
 
 const searchInput: HTMLInputElement = document.querySelector('#search') as HTMLInputElement;
+const resultElement: HTMLUListElement = document.querySelector('#result') as HTMLUListElement;
 
 const sequence$: Observable<Event> = fromEvent(searchInput, 'input');
 
-const sequence1$: Observable<IGithubUrl[]> = sequence$.pipe(
+const sequence1$: Observable<IGithubUrl> = sequence$.pipe(
     debounceTime(300),
     map((event: Event): string => {
         return (event.target as HTMLInputElement).value;
     }),
+    filter((value: string) => value !== ''),
     distinctUntilChanged(),
-    switchMap((value: string): IGithubResponse => {
-
+    switchMap((value: string): Observable<IGithubUrl[]> => {
         return from(
             fetch(`https://api.github.com/search/repositories?q=${value}`)
                 .then((response: Response): object => response.json())
                 .then((response: IGithubResponse): IGithubUrl[] => response.items)
         ).pipe(
+            map((items: IGithubUrl[]) => items.length ? items : []),
             catchError(() => {
                 return of([]);
             })
         );
-
     }),
-    switchMap((items: IGithubUrl[]): IGithubUrl => {
+    switchMap((items: IGithubUrl[]): Observable<IGithubUrl|null> => {
         return from(items).pipe(
+            map((item: IGithubUrl) => item),
             catchError(() => {
-                return of([]);
+                return of(null);
             })
         );
     })
 );
 
-sequence1$.subscribe((value: IGithubUrl) => {
-    console.log(value);
+sequence$.pipe(debounceTime(1000), distinctUntilChanged()).subscribe((_event: Event) => {
+    resultElement.innerText = '';
+});
+
+sequence1$.subscribe((item: IGithubUrl|null) => {
+    if (!item) {
+        return;
+    }
+    const li: HTMLLIElement = document.createElement('li');
+    li.innerHTML = `<a href=${item.html_url} target="_blank">${item.name}</a>`;
+    resultElement.appendChild(li);
 });
